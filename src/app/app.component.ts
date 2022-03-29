3f7e1c65cf96c404ce3d09fa3baf1b049649c97e
galczo5 <kamil.galek@gmail.com>
2022-03-29 20:31:18 +0200

    application without zones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import {Component} from '@angular/core';
+import {ApplicationRef, ChangeDetectionStrategy, Component, OnDestroy} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter, Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,34 @@ import {Component} from '@angular/core';
     <div style="padding: 4px 25px;">
       <router-outlet></router-outlet>
     </div>
-  `
+  `,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+
+  private readonly onDestroy$: Subject<void> = new Subject<void>();
+
+  constructor(
+    router: Router,
+    applicationRef: ApplicationRef
+  ) {
+    this.zonelessRouterStarter(router, applicationRef);
+  }
+
+  ngOnDestroy() {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  private zonelessRouterStarter(router: Router, applicationRef: ApplicationRef): void {
+    router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        takeUntil(this.onDestroy$)
+      )
+      .subscribe(() => {
+        applicationRef.tick();
+      });
+  }
+
 }
